f28e69827a801c82d442dc0ba937edb38a04ff9a
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-02-08 14:11:57 +0100

    add font awsome

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 import './../../css/Navbar.css';
 
 function Navbar(){
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click)
+
     return (
         <>
         <nav className='navbar'>
             <Link to='/' className='navbar-logo'>Iskierka</Link>
+            <div className='menu-icon' onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
         </nav>
         </>
     );
