be3e25db37d8ae31e9dff3d94834c15a8da5ebbb
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-02-09 19:43:37 +0100

    fix navbar

--- src/components/MenuItems/MenuItems.js
+++
@@ -1,6 +0,0 @@
-export const MenuItems = [
-    {title: 'Test', path: '/home', cName: 'dropdown-link'},
-    {title: 'Test', path: '/', cName: 'dropdown-link'},
-    {title: 'Test', path: '/', cName: 'dropdown-link'},
-    {title: 'Test', path: '/', cName: 'dropdown-link'}
-]
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,17 @@
 import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import './../../css/Navbar.css';
-import Dropdown from './../Dropdown/Dropdown';
-import Button from './../Button/Button';
 
 function Navbar(){
 
     const [click, setClick] = useState(false);
-    const [dropdown, setDropdown] = useState(false);
+
 
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false);
 
-    const onMouseEnter = () => {
-        if(window.innerWidth < 960 ){
-            setDropdown(false)
-        } else {
-            setDropdown(true)
-        }
-    }
-    const onMouseLeave = () => {
-        if(window.innerWidth < 960 ){
-            setDropdown(false)
-        } else {
-            setDropdown(false)
-        }
-    }
+
+
 
     return (
         <>
@@ -40,12 +26,6 @@ function Navbar(){
                         Home
                     </Link>
                 </li>
-                <li className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-                    <Link to='/teams' className="nav-links" onClick={closeMobileMenu}>
-                        Teams <i className='fas fa-caret-down'></i>
-                    </Link>
-                    {dropdown && <Dropdown />}
-                </li>
                 <li className='nav-item'>
                     <Link to='/' className="nav-links" onClick={closeMobileMenu}>
                         Home
@@ -54,15 +34,10 @@ function Navbar(){
                 <li className='nav-item'>
                     <Link to='/' className="nav-links" onClick={closeMobileMenu}>
                         Home
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>
-                        Sign up
                     </Link>
                 </li>
             </ul>
-            <Button />
+            
         </nav>
         </>
     );
