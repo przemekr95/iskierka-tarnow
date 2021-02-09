b6eba8ea18fbeae0e5f1060bfe6886b9d5c313ed
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-02-09 10:44:30 +0100

    add navbar

---
+++ src/components/MenuItems/MenuItems.js
@@ -0,0 +1,6 @@
+export const MenuItems = [
+    {title: 'Test', path: '/home', cName: 'dropdown-link'},
+    {title: 'Test', path: '/', cName: 'dropdown-link'},
+    {title: 'Test', path: '/', cName: 'dropdown-link'},
+    {title: 'Test', path: '/', cName: 'dropdown-link'}
+]
